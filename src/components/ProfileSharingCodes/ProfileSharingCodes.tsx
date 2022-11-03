d49e60d91c954bcb9a52e7acd7cb76b1a1a94474
Mike Alizade <mike@sciondigital.co.uk>
2022-11-03 17:41:12 +0000

    remove commented out code

--- src/components/ProfileSharingCodes/ProfileSharingCodes.tsx
+++ src/components/ProfileSharingCodes/ProfileSharingCodes.tsx
@@ -1,73 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import type { NextPage } from 'next'
-import { useSelector } from 'react-redux'
-import { RootState } from '@state/store'
-import { FormProvider, useForm, FieldValues } from 'react-hook-form'
-import { useDrawer } from '@hooks/useDrawer'
-import { Input } from '@components/Input/Input'
-import { FormButtons } from '@components/FormButtons/FormButtons'
-import { useUser } from '@auth0/nextjs-auth0'
-import Image from 'next/image'
-import { fetcher } from '@utils/functions'
-import { Button } from '@components/Button/Button'
 import { Tabs as StylesTabs } from '@components/Tabs/Tabs.style'
 import * as S from '@components/ProfileSharingCodes/ProfileSharingCodes.style'
 
 import { Tabs } from '@components/Tabs/Tabs'
 
-export const ProfileSharingCodes: NextPage<{ data?: any; disabled?: boolean }> = ({
-  data,
-  disabled,
-}): JSX.Element => {
-  // const user = useSelector((state: RootState) => state.user)
-  // const methods = useForm()
-  // const { toggleDrawer } = useDrawer()
-  // const { handleSubmit, reset } = methods
-  // const { user: { sub, nickname = '', picture = '' } = {}, isLoading = false } = useUser()
-  // const {
-  //   profile: {
-  //     summary: {
-  //       proudActions = '',
-  //       campaigns = '',
-  //       switchingStatement = '',
-  //       username = '',
-  //       location = '',
-  //     } = {},
-  //   } = {},
-  // } = user
-
-  // const onSubmit = async (data: FieldValues): Promise<void> => {
-  //   console.log('>> data', data)
-  //   await save(data)
-  //   toggleDrawer('')()
-  // }
-
-  // const onCancel = (): void => {
-  //   reset()
-  //   toggleDrawer('')()
-  // }
-
-  // const save = async (data): Promise<void> => {
-  //   const body = {
-  //     filter: { sub },
-  //     payload: { $set: { [`profile.summary`]: data } },
-  //     collection: 'users',
-  //     upsert: false,
-  //   }
-
-  //   await fetcher(`/api/db/updateOne`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(body),
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   reset && reset(data)
-  // }, [data, reset, toggleDrawer])
-
+export const ProfileSharingCodes: NextPage = (): JSX.Element => {
   const onCancel = () => {
     //
   }
