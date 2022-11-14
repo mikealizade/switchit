9d901d0c85a49eb1f840dc6289c03ee0caba24e3
Mike Alizade <mike@sciondigital.co.uk>
2022-11-14 16:14:40 +0000

    attempt to prerender random blog page

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Blog from '@modules/Blog/Blog'
 import { useRouter } from 'next/router'
-// import { baseUrl } from '@utils/constants'
+import { baseUrl } from '@utils/constants'
 import { Posts, Post } from '@pages/dashboard'
 
 export async function getStaticPaths() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/db/findRandomPost`)
+  const res = await fetch(`${baseUrl}/api/db/findRandomPost`)
   const [post] = await res.json()
   // const paths = posts.map(({ id }: { id: string }) => ({ params: { id } }))
 
@@ -20,7 +20,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context: any) {
   const id = context.params.id
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/db/findPost?id=${id}`)
+  const res = await fetch(`${baseUrl}/api/db/findPost?id=${id}`)
   const posts = await res.json()
 
   return { props: { posts } }
