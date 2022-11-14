86d818e1dfa2147f9496882cc7cbbea6560caba9
Mike Alizade <mike@sciondigital.co.uk>
2022-11-14 15:55:28 +0000

    attempt to prerender random blog page

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -4,18 +4,18 @@ import { useRouter } from 'next/router'
 // import { baseUrl } from '@utils/constants'
 import { Posts, Post } from '@pages/dashboard'
 
-// export async function getStaticPaths() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/db/findRandomPost`)
-//   const posts = await res.json()
-//   const paths = posts.map(({ id }: {id:string}) => ({ params: { id } }))
-//   return {
-//     paths,
-//     fallback: true,
-//     //The paths that have not been generated at build time will not result in a 404 page.
-//     //Instead, fallback: true This will be used to automatically render
-//     //the page with the required props.
-//   }
-// }
+export async function getStaticPaths() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/db/findRandomPost`)
+  const posts = await res.json()
+  const paths = posts.map(({ id }: { id: string }) => ({ params: { id } }))
+  return {
+    paths,
+    fallback: true,
+    //The paths that have not been generated at build time will not result in a 404 page.
+    //Instead, fallback: true This will be used to automatically render
+    //the page with the required props.
+  }
+}
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/db/findRandomPost`)
