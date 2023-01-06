10b3b217f4ef218b5f87a58065e2d3837abd8049
Mike Alizade <mike@sciondigital.co.uk>
2023-01-06 10:27:05 +0000

    improved video functionality

--- src/pages/switching/choose-bank-delete.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import { ChooseBank } from '@modules/Switching/ChooseBank-delete'
-
-const ChooseBankPage: NextPage = () => {
-  return <ChooseBank />
-}
-
-export default ChooseBankPage
