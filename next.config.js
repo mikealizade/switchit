e7df73712789809fc0ce570723ee5e1ea02be3f1
Mike Alizade <mike@sciondigital.co.uk>
2023-02-09 08:12:42 +0000

    add sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -22,3 +29,5 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: true })
