8c50dbbfc00708a518c6c9a8daaba3ddc74aa5da
Mike Alizade <mike@sciondigital.co.uk>
2023-02-27 17:45:32 +0000

    og

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,13 +54,12 @@ const App = ({ Component, pageProps }: AppProps) => {
             name='description'
             content='Switch to a green bank on our Bank Switching Platform'
           />
-
-          <meta property='og:title' content='Switch It Green | Green Bank Switching Platform' />
+          <meta property='og:title' content='Switch It Green || Green Bank Switching Platform' />
           <meta
             property='og:description'
             content='Switch to a green bank on our Bank Switching Platform'
           />
-          <meta property='og:image' content='/switchit_logo.png' />
+          <meta property='og:image' content='https://switchit-weld.vercel.app/switchit_logo.png' />
 
           <link rel='icon' href='/favicon.ico' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
